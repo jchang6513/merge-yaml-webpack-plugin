00a1e0792b98ee4e13d1f68261fc82415a6dacc5
Yi-Wei Chang <jchang6513@gmail.com>
2019-09-13 15:30:39 +0800

    🎉 initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+class MergeYamlWebpackPlugin {
+  constructor(options) {
+    if (!options.files || !options.files.length) {
+        throw new Error('MergeYamlWebpackPlugin: option files is required and should not be empty');
+    }
+    this.settings = options;
+  }
+
+  mergeFiles() {
+    const { fileName, outputPath, root, files } = this.settings;
+    const datas = {}
+    files.forEach((file) => {
+      const data = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
+      datas = { ...datas, ...data };
+    })
+
+    const result = (!root) ? { [root]: datas } : datas;
+
+    fs.writeFile(`${outputPath}/${fileName}`, yaml.safeDump(result), (err) => {
+      if (err) { console.log(err); }
+    });
+  }
+
+  apply(compiler) {
+    let compileLoopStarted = false;
+    compiler.hooks.emit.tap('MergeYamlWebpackPlugin', () => {
+      if (!compileLoopStarted) {
+        compileLoopStarted = true;
+        this.mergeFiles();
+      }
+    });
+    compiler.hooks.afterEmit.tap('MergeYamlWebpackPlugin', () => {
+      compileLoopStarted = false;
+    });
+  }
+}
+
+module.exports = MergeYamlWebpackPlugin;
